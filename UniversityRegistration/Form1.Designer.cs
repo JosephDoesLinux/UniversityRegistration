ea89c71e5683960599c556b5c9dbd3caa358ed60
Joseph <Joseph@RDPWINDOWS>
2026-01-04 15:00:17 +0200

    Login UI done, now for the logic

--- UniversityRegistration/Form1.Designer.cs
+++ UniversityRegistration/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            groupBox1 = new GroupBox();
+            btn_login = new Button();
+            txt_password = new TextBox();
+            txt_id = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            link_ums = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(244, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(151, 152);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(link_ums);
+            groupBox1.Controls.Add(btn_login);
+            groupBox1.Controls.Add(txt_password);
+            groupBox1.Controls.Add(txt_id);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(226, 152);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Enter Credentials for Registration";
+            // 
+            // btn_login
+            // 
+            btn_login.Location = new Point(137, 123);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(75, 23);
+            btn_login.TabIndex = 2;
+            btn_login.Text = "Login";
+            btn_login.UseVisualStyleBackColor = true;
+            // 
+            // txt_password
+            // 
+            txt_password.Location = new Point(81, 64);
+            txt_password.Name = "txt_password";
+            txt_password.Size = new Size(131, 23);
+            txt_password.TabIndex = 1;
+            txt_password.UseSystemPasswordChar = true;
+            // 
+            // txt_id
+            // 
+            txt_id.Location = new Point(81, 22);
+            txt_id.Name = "txt_id";
+            txt_id.Size = new Size(131, 23);
+            txt_id.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(9, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Password  :";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(9, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 5;
+            label1.Text = "ID               :";
+            // 
+            // link_ums
+            // 
+            link_ums.AutoSize = true;
+            link_ums.Location = new Point(6, 127);
+            link_ums.Name = "link_ums";
+            link_ums.Size = new Size(97, 15);
+            link_ums.TabIndex = 7;
+            link_ums.TabStop = true;
+            link_ums.Text = "LIU UMS Website";
+            link_ums.LinkClicked += link_ums_LinkClicked;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(407, 176);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+        private PictureBox pictureBox1;
+        private GroupBox groupBox1;
+        private Button btn_login;
+        private TextBox txt_password;
+        private TextBox txt_id;
+        private Label label2;
+        private Label label1;
+        private LinkLabel link_ums;
     }
 }
--- UniversityRegistration/Form1.cs
+++ UniversityRegistration/Form1.cs
@@ -1,10 +1,25 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
 namespace UniversityRegistration
+
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        static string connString = "Data Source= RDPWindows\\SQLEXPRESS;" +
+            " Initial Catalog=MarketDB; Integrated Security=True;" +
+            " TrustServerCertificate=True";
+
+        SqlConnection conn = new SqlConnection(connString);
+
+
+
+        private void link_ums_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            InitializeComponent();
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://syslb.liu.edu.lb/login/",
+                UseShellExecute = true
+            });
         }
     }
 }
